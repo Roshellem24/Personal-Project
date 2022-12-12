c1bc711f3a0f1e555f1fe1556ae2f42727419882
Roshellem24 <98101425+Roshellem24@users.noreply.github.com>
2022-12-12 12:38:09 -0500

    :>

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,24 +11,43 @@ public class Player : MonoBehaviour
 
     public float rotationSpeed;
 
+    Animator chickenAnim;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        chickenAnim = GetComponent<Animator>();
+    }
 
+    void Update()
+    {
+         // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis ("Horizontal");
+
+         // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed * verticalInput);
+
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.up * rotationSpeed * horizontalInput * Time.deltaTime);
+
+
+        if(verticalInput > 0 || verticalInput < 0)
+        {
+            chickenAnim.SetBool("Walk", true);
+        }else if (verticalInput == 0)
+        {
+            chickenAnim.SetBool("Walk", false);
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        // get the user's vertical input
-        verticalInput = Input.GetAxis("Vertical");
-        horizontalInput = Input.GetAxis ("Horizontal");
+       
 
-        // move the plane forward at a constant rate
-        transform.Translate(Vector3.forward * speed * verticalInput);
-
-        // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.up * rotationSpeed * horizontalInput * Time.deltaTime);
+       
     }
 }
